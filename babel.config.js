1498e713c914596cceaf9d3691d97125c6c664d3
juhyeong <juhyeongee@gmail.com>
2022-10-24 12:04:23 +0900

    feat: install babel resolver, baseURL&path setting

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,34 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
+          ],
+          alias: {
+            "@": "./src",
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@constants": "./src/constants",
+            "@navigations": "./src/navigations",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
