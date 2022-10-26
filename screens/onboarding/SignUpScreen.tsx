b51cdbe8355c04ea8ee96fcb58856100545b3766
juhyeong <juhyeongee@gmail.com>
2022-10-26 16:52:14 +0900

    feat: adjust onChangeText with input-textValue by new inputChangeHandler

--- screens/onboarding/SignUpScreen.tsx
+++ screens/onboarding/SignUpScreen.tsx
@@ -15,24 +15,42 @@ export default function SignUpScreen() {
     inputIdentifier: string,
     wantToChangeValue: string
   ) => {
+    console.log("inputIdentifier", inputIdentifier);
+    console.log("wantToChangeValue", wantToChangeValue);
     setInputValues((currentInputValue) => {
       return { ...currentInputValue, [inputIdentifier]: wantToChangeValue };
     });
   };
-
+  //여러 인풋이 있는데, 이것에 대한 각각의 핸들러 함수가 필요함
+  // 그렇다고 함수를 다 각각 만드는 것은 좀 비효율적인 일
+  // 그래서 이것을 extends 가능한 함수로 만드는 것이 좋겠음.
   return (
     <View style={styles.container}>
       <View style={{ flex: 0.2 }} />
       <View style={styles.inputAreaContainer}>
         <InputArea
           title="이메일"
-          textInputConfig={{ placeholder: "abcd@asdf.com" }}
+          textInputConfig={{
+            placeholder: "abcd@asdf.com",
+            onChangeText: inputChangeHandler.bind(this, "email"),
+            value: inputValues.email,
+          }}
         />
         <InputArea
           title="비밀번호"
-          textInputConfig={{ placeholder: "10글자 이상 입력해주세요" }}
+          textInputConfig={{
+            placeholder: "10글자 이상 입력해주세요",
+            onChangeText: inputChangeHandler.bind(this, "password"),
+            value: inputValues.password,
+          }}
         />
-        <InputArea title="비밀번호 확인" />
+        <InputArea
+          title="비밀번호 확인"
+          textInputConfig={{
+            onChangeText: inputChangeHandler.bind(this, "passwordConfirm"),
+            value: inputValues.password,
+          }}
+        />
       </View>
       <View style={{ flex: 0.2, width: "100%" }}>
         <PrimaryBtn onPress={() => console.log("pressed")} text="다음" />
