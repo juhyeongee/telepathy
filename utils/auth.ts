b07427d4620fd632bdc28803b13a4489ef4edb93
juhyeong <juhyeongee@gmail.com>
2022-11-03 23:17:09 +0900

    feat: 이메일 로그인 기능 구현 완료

--- utils/auth.ts
+++ utils/auth.ts
@@ -6,13 +6,21 @@ interface ICreateUser {
   password: string;
 }
 
+const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  const result = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(`${mode} 완료`);
+};
+
 export async function createUser(email, password) {
-  axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
